ee4bb56fefda69dcf15609e91ff8d26cb421828f
pnouhet <83618563+pnouhet@users.noreply.github.com>
2025-10-28 22:05:32 +0100

    Add files via upload

---
+++ src/Lecteurs/AbstractFile.java
@@ -0,0 +1,79 @@
+package Lecteurs;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class AbstractFile implements IAbstractFile
+{
+	protected String cheminFichier;
+	protected ArrayList<String> contenu;
+	
+	public AbstractFile(String cheminFichier)
+	{
+		this.cheminFichier = cheminFichier;
+	}
+	
+	@Override
+	public void lireFichier()
+	{
+		this.contenu = lectureDuFichier();
+	}
+	
+	protected ArrayList<String> lectureDuFichier() 
+	{
+		ArrayList<String> lignes = new ArrayList<>();
+		try
+		{
+			lignes.addAll(Files.readAllLines(Paths.get(cheminFichier)));
+		}
+		catch (IOException e)
+		{
+			System.err.println("Erreur fichier : " + e.getMessage() + " est introuvable.");
+		}
+		return lignes;
+		/*File f = new File(cheminFichier);
+		try {
+		      FileInputStream in = new FileInputStream(f);
+		      int i = in.read();
+		      while (i != -1) {
+		    	  System.out.println((char) i);
+		    	  i = in.read();
+		      }
+		      in.close();
+		    } catch (Exception e) {
+		      e.getStackTrace();
+		}*/
+	}
+	
+	public void afficherFichier()
+	{
+		for(String ligne : contenu) 
+		{
+			System.out.println(ligne);
+		}
+	}
+	
+	public void inverserFichier()
+	{
+		Collections.reverse(contenu);
+		for(String ligne : contenu) 
+		{
+			System.out.println(ligne);
+		}
+		Collections.reverse(contenu);
+		
+	}
+	
+	public void creerPalindrome()
+	{
+		for (String ligne : contenu)
+		{			
+			StringBuffer palindrome = new StringBuffer(ligne);
+			palindrome.reverse();
+			System.out.println(palindrome);
+		}
+	}
+}
---
+++ src/Lecteurs/IAbstractFile.java
@@ -0,0 +1,9 @@
+package Lecteurs;
+
+public interface IAbstractFile 
+{
+	void lireFichier(); //lire fichier
+	void afficherFichier(); //afficher contenu du fichier
+	void inverserFichier(); //inverser les lignes du fichier
+	void creerPalindrome(); //inverser les caracteres du fichier
+}
